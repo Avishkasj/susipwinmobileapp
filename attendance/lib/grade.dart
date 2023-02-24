9b0f095550f8aa5bc2857ab790ed50c522782cd1
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-02-24 23:02:46 +0530

    select grade

--- attendance/lib/grade.dart
+++ attendance/lib/grade.dart
@@ -1,6 +1,7 @@
 import 'package:attendance/QrScanner.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Grade extends StatelessWidget {
   @override
@@ -10,6 +11,8 @@ class Grade extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: Color.fromRGBO(7, 20, 48, 1),
           leading: Padding(
             padding: const EdgeInsets.only(left: 16),
             child: Image.asset('assets/susipwinlogo.png'),
@@ -19,104 +22,222 @@ class Grade extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder:(context)=>QrScanner()));
-                  String datetime = DateTime.now().toString();
-                  print(datetime);
-                  print("Course 3 clicked");
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: Colors.deepPurpleAccent,
-                    ),
-                    height: 100.0,
-                    // height: 100,
-                    // width: double.infinity,
-                    // color: Colors.green,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Information communication technology",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => QrScanner()));
+                      String datetime = DateTime.now().toString();
+                      print(datetime);
+                      print("Course 3 clicked");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    color: Colors.redAccent,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                      width: 5.0,
+                                    )),
+                                height: 100,
+                                width: 130,
+                                child: Center(
+                                  child: Column(
+                                    children: [
+                                      Icon(Icons.add_business),
+                                      Text(
+                                        "A/L",
+                                        style: TextStyle(
+                                          fontSize: 30,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Row(
-                              children: [
-                                Text("Teacher  "),
-                                Text("J.M.A.S. Jayasooriya"),
-                              ],
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-
-
-
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder:(context)=>QrScanner()));
-                  String datetime = DateTime.now().toString();
-                  print(datetime);
-                  print("Course 3 clicked");
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: Colors.blue,
-                    ),
-                    height: 100.0,
-                    // height: 100,
-                    // width: double.infinity,
-                    // color: Colors.green,
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Biosystem technology",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => QrScanner()));
+                      String datetime = DateTime.now().toString();
+                      print(datetime);
+                      print("Course 4 clicked");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    color: Colors.redAccent,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                      width: 5.0,
+                                    )),
+                                height: 100,
+                                width: 130,
+                                child: Center(
+                                  child: Text(
+                                    "O/L",
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Row(
-                              children: [
-                                Text("Teacher  "),
-                                Text("J.M.A.S. Jayasooriya"),
-                              ],
-                            ),
-                          ),
-                        ],
+
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
 
+
+//second row
+
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => QrScanner()));
+                      String datetime = DateTime.now().toString();
+                      print(datetime);
+                      print("Course 5 clicked");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    color: Colors.redAccent,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                      width: 5.0,
+                                    )),
+                                height: 100,
+                                width: 140,
+                                child: Center(
+                                  child: Column(
+                                    children: [
+                                      Icon(Icons.add_business),
+                                      Text(
+                                        "5",
+                                        style: TextStyle(
+                                          fontSize: 30,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => QrScanner()));
+                      String datetime = DateTime.now().toString();
+                      print(datetime);
+                      print("Course 6 clicked");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(0.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    color: Colors.redAccent,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                      width: 5.0,
+                                    )),
+                                height: 100,
+                                width: 140,
+                                child: Center(
+                                  child: Text(
+                                    "6",
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
+        ),
+        bottomNavigationBar: GNav(
+          backgroundColor: Color.fromRGBO(7, 20, 48, 1),
+          color: Colors.white,
+          activeColor: Colors.white,
+          tabs: [
+            GButton(
+              icon: Icons.home,
+              text: ("Home"),
+            ),
+            GButton(
+              icon: Icons.select_all,
+              text: ("Courses"),
+            ),
+            GButton(
+              icon: Icons.settings,
+              text: ("Course"),
+            ),
+            GButton(
+              icon: Icons.logout,
+              text: "Log out",
+            ),
+          ],
         ),
       ),
     );
