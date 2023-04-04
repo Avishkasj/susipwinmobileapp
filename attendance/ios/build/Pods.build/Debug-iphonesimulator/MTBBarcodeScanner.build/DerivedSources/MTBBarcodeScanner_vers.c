b88a9d7fbecb1461955c8c72cdd54b0635f4e50d
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-04 17:27:37 +0530

    IOS coco pod test

---
+++ attendance/ios/build/Pods.build/Debug-iphonesimulator/MTBBarcodeScanner.build/DerivedSources/MTBBarcodeScanner_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char MTBBarcodeScannerVersionString[];
+ extern const double MTBBarcodeScannerVersionNumber;
+
+ const unsigned char MTBBarcodeScannerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MTBBarcodeScanner  PROJECT:Pods-1" "\n";
+ const double MTBBarcodeScannerVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ attendance/ios/build/Pods.build/Debug-iphonesimulator/qr_code_scanner.build/DerivedSources/qr_code_scanner_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char qr_code_scannerVersionString[];
+ extern const double qr_code_scannerVersionNumber;
+
+ const unsigned char qr_code_scannerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:qr_code_scanner  PROJECT:Pods-1" "\n";
+ const double qr_code_scannerVersionNumber __attribute__ ((used)) = (double)1.;
