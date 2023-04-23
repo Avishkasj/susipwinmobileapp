be7dc24302176313c13be1cd6ba484b8d33314c9
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-23 19:41:11 +0530

    Update register.dart

--- attendance/lib/register.dart
+++ attendance/lib/register.dart
@@ -11,10 +11,52 @@ class _registerState extends State<register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("data"),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text("data"),
+
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Image(image: AssetImage('assets/susipwinlogo.png')),
+            ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+
+            Center(
+              child: Text(
+                "Susipwin",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+
+
+        Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.redAccent),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextFormField(
+                          validator: (val) =>
+                              val == "" ? "Please Enter E-mail" : null,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
